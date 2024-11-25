ab740d4d3bd72303fba6023f46446478293ec9ed
nellyhuima <nelly.huima@icloud.com>
2024-11-25 18:34:34 +0200

    app.get added to server.js. Works!

--- server.js
+++ server.js
@@ -5,3 +5,19 @@ const app = express();
 app.listen(3001, () => {
     console.log('server running in port 3001');
 });
+
+app.get('/genre', (req, res) => {
+    res.send('This is genre page')
+});
+
+app.get('/movie', (req, res) => {
+});
+
+app.get('/review', (req, res) => {
+});
+
+app.get('/user', (req, res) => {
+});
+
+app.get('/favorite', (req, res) => {
+});
