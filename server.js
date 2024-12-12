3b40fafbf1ea55de86177d92e7f8833544a5c793
nellyhuima <nelly.huima@icloud.com>
2024-12-12 18:50:41 +0200

    connection implementation successful

--- server.js
+++ server.js
@@ -1,44 +1,38 @@
 import express from 'express';
+import pg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
-
 const port = process.env.PORT || 3001;
-
-
-
-
-
-const { Client } = require('pg');
-
-// Creating a new PostgreSQL client
-const client = new Client({
-  host: 'localhost',
-  port: 5432,
-  user: 'postgres',
-  password: '',
-  database: '-Database-management-and-programming'
-});
-
-client.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err.stack);
-  } else {
-    console.log('Connected to the PostgreSQL database');
-  }
-});
-
-
-
-
 
 app.listen(port, ()=>{
     console.log(`server running in port ${port}`) //console.log if wanting to write something in terminal
 });
 
-app.get('/', (req, res)=> res.send("Hello world!")); //res.send if wanting to write something on the page
+const pgPool = new pg.Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+});
 
-app.get('/genre', (req, res)=> {
-    res.send("genres go here!");
-    res.send()
+export {pgPool};
+
+
+
+app.get('/', (req, res)=> res.send("Hello world!")); //res.send if wanting to write something on the page.
+
+app.get('/genre', async (req, res)=> {
+  res.send("genres go here!");
+    try {
+      const result = await pgPool.query('SELECT * FROM genre');
+
+      res.json(result.rows);
+    } catch (error) {
+      console.error('Error fetching genres:', error.message);
+      res.status(500).send(`Internal server error: ${error.message}`);
+    }
 });
 
 app.get('/movie', (req, res)=> {
